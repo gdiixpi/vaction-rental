cb6790cbadda9cb3b30bbb5d5e0e6e11e5d49614
The Devloper <73634258+thepurvik@users.noreply.github.com>
2023-02-23 17:10:28 +0530

    Add files via upload

---
+++ src/app/pages/extrapages/blog-grid/data.ts
@@ -0,0 +1,127 @@
+/**
+ * News List
+ */
+ const newsData = [
+  {
+    image: 'assets/img/real-estate/blog/01.jpg',
+    sub_title: "Tips &amp; Advice",
+    title: "Looking for a New Place? Use This Time to Create Your Wishlist",
+    content: "Nulla felis neque ultrices ut aliquam. Pellentesque id semper iaculis scelerisque etiam egestas interdum proin sit. Ornare venenatis, ullamcorper amet arcu ipsum ut morbi enim. Senectus quam egestas facilisi enim diam posuere ultricies interdum sed. Amet, risus eros cursus vitae, sit?",
+    profile: 'assets/img/avatars/06.jpg',
+    name: 'Kristin Watson',
+    date: 'Mar 13',
+    comments: "No",
+    category:""
+  },
+  {
+    image: 'assets/img/real-estate/blog/02.jpg',
+    sub_title: "Industry News",
+    title: "Traditional Real Estate Marketing Holds Firm Despite Pandemic",
+    content: "Phasellus cursus orci pulvinar proin nulla vitae. Arcu faucibus vestibulum fames eget nunc. Ut in sed platea egestas amet purus nibh suspendisse semper. Senectus quam egestas facilisi enim diam posuere ultricies interdum sed. Amet, risus eros cursus vitae, sit. Nulla felis neque ultrices ut aliquam.",
+    profile: 'assets/img/avatars/04.jpg',
+    name: 'Darrell Steward',
+    date: 'Feb 27',
+    comments: "3",
+    category:""
+  },
+];
+
+/**
+ * Blogs Data List
+ */
+ const blogsData = [
+  {
+    btn: "new",
+    btn_color: "bg-info",
+    image: "assets/img/real-estate/blog/03.jpg",
+    sub_title: "Travel/Attractions",
+    title: "5 Must-See Attractions in Davenport, Florida",
+    content: `
+    If you're planning a vacation to Davenport, Florida, there's no shortage of exciting things to do and see. From theme parks to natural wonders, this small town has something for everyone. Here are our top picks for must-see attractions in Davenport:
+
+1. Walt Disney World Resort: No trip to Florida is complete without a visit to the "Happiest Place on Earth." With four theme parks, two water parks, and endless dining and shopping options, there's always something new and exciting to discover at Disney.
+
+2. Legoland Florida Resort: This theme park is perfect for families with young children, as it features over 50 rides, shows, and attractions designed just for kids. Kids can build and race their own Lego cars, meet their favorite Lego characters, and even stay overnight in a Lego-themed hotel room.
+
+3. Gatorland: This 110-acre theme park is home to over 4,000 alligators and crocodiles, as well as a host of other exotic animals. Visitors can watch thrilling alligator wrestling shows, take a guided tour of the park, or even zip line over the gators.
+
+4. Bok Tower Gardens: If you're looking for a more relaxed and natural attraction, Bok Tower Gardens is the perfect spot. This beautiful botanical garden features a 205-foot tall bell tower, a series of landscaped gardens, and a museum showcasing the history of the gardens.
+
+5. Celebration Golf Club: Golfers will love this stunning 18-hole course, designed by Robert Trent Jones Sr. and Jr. The course features rolling fairways, challenging bunkers, and beautiful views of the surrounding wetlands and forests.
+
+6. Whether you're an adventure seeker or just looking to relax and enjoy nature, Davenport has something for everyone. With these top attractions, you're sure to have a vacation to remember.
+    `,
+    profile: 'assets/img/avatars/07.jpg',
+    name: 'Cody Fisher',
+    date: 'Dec 28',
+    comments: "0",
+    category:"Home"
+  },
+  {
+    btn: "",
+    btn_color: "",
+    image: "assets/img/real-estate/blog/04.jpg",
+    sub_title: "Inspiration",
+    title: "5 Easy-to-Ambitious Projects to Improve Your Home",
+    content: "Phasellus consequat ut sagittis, vestibulum, eget egestas senectus quis congue. Posuere montes, in ultrices vitae, luctus. Ultrices aliquet pellentesque eget...",
+    profile: 'assets/img/avatars/03.jpg',
+    name: 'Kathryn Murphy',
+    date: 'Dec 26',
+    comments: "4",
+    category:"Inspiration"
+  },
+  {
+    btn: "",
+    btn_color: "",
+    image: "assets/img/real-estate/blog/05.jpg",
+    sub_title: "Inspiration",
+    title: "How Real Estate Drone Photography Can Elevate Your Listing?",
+    content: "Nunc vivamus nam vitae ut faucibus metus pretium nisl. Elementum sed tincidunt nec aliquam tempus. Neque pharetra vitae, mauris malesuada adipiscing...",
+    profile: 'assets/img/avatars/06.jpg',
+    name: 'Kristin Watson',
+    date: 'Dec 12',
+    comments: "3",
+    category:"Inspiration"
+  },
+  {
+    btn: "",
+    btn_color: "",
+    image: "assets/img/real-estate/blog/06.jpg",
+    sub_title: "Tips &amp; Advice",
+    title: "7 Tips for Achieving Maximum Coziness",
+    content: "Enim faucibus mattis feugiat porttitor a arcu quisque. Donec massa volutpat dolor id eget consectetur viverra. Etiam consectetur mauris, scelerisque gravida convallis...",
+    profile: 'assets/img/avatars/04.jpg',
+    name: 'Darrell Steward',
+    date: 'Dec 3',
+    comments: "1",
+    category:"Tips"
+  },
+  {
+    btn: "",
+    btn_color: "",
+    image: "assets/img/real-estate/blog/07.jpg",
+    sub_title: "Industry Views",
+    title: "How To Modernize Your Home on a Budget",
+    content: "Phasellus cursus orci pulvinar proin nulla vitae. Arcu faucibus vestibulum fames eget nunc. Ut in sed platea egestas amet purus nibh suspendisse semper...",
+    profile: 'assets/img/avatars/03.jpg',
+    name: 'Kathryn Murphy',
+    date: 'Oct 17',
+    comments: "2",
+    category:"Industry News"
+  },
+  {
+    btn: "",
+    btn_color: "",
+    image: "assets/img/real-estate/blog/08.jpg",
+    sub_title: "Property Market Analysis",
+    title: "When Is the Right Time to Downsize Your Home?",
+    content: "Parturient purus non consectetur eu arcu, feugiat eget vitae. Tellus at sociis egestas magna integer. Vel sagittis morbi ultrices turpis neque sed venenatis enim laoreet...",
+    profile: 'assets/img/avatars/07.jpg',
+    name: 'Cody Fisher',
+    date: 'Nov 15',
+    comments: "0",
+    category:"Market Analysis"
+  },
+];
+
+export { newsData, blogsData };
